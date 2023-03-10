5befdc80a52adcbf6fc457b58a57d909cf952911
llemanhbin <llemanhbin@gmail.com>
2023-03-10 09:06:30 +0700

    upload json db

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,6 @@
+/*
 export const BE_URL = "http://localhost:4000/";
+*/
+export const BE_URL = "https://json-server-vercel-mr13xpfhc-lemanhbin.vercel.app/";
 export const KEY_ACCESS_TOKEN = 'accessToken';
 export const KEY_IS_LOGGER = "isLogin"
